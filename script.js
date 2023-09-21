3ec078eb87652f960c05a91836d9a0b6b7edb7a6
Marek Wegierek <mpwegierek@gmail.com>
2023-09-21 16:24:08 +0200

    Add conditional logic to improve UX

--- script.js
+++ script.js
@@ -46,31 +46,35 @@ let currentTime = 0
 
 /* 'data' stores information about single event/time spent on task */
 
-let data = {
-  time: "12:05",
-  secs: 52,
-  mins: 13,
-  hours: 1,
-  status: "JS Tasks",
-}
+let data = {}
 
 /*cardData stores information about all events from day, 
 together with current date
 */
 
 let lsCardData = JSON.parse(localStorage.getItem("tasklist_serialized"))
-
 let cardData = {
   day: cardDay,
-  taskList: [],
+  taskList: [
+    {
+      time: "8:00",
+      secs: 0,
+      mins: 0,
+      hours: 0,
+      status: "nameOfTask",
+    },
+  ],
 }
-if (cardData.taskList != lsCardData) {
+
+if (!lsCardData) {
+  let x = JSON.stringify(cardData)
+  localStorage.setItem("tasklist_serialized", x)
+} else if (cardData.taskList != lsCardData) {
   cardData.taskList = lsCardData
 }
 
 let taskArray = cardData.taskList.map((taskInfo) => {
   let { time, secs, mins, hours, status } = taskInfo
-
   return `<li>
   <p class="card0-list-timeStamp">${time}</p>
   <p class="card0-list-job">${status}</p>
@@ -78,11 +82,8 @@ let taskArray = cardData.taskList.map((taskInfo) => {
   </li>`
 })
 let taskArrayHTML = taskArray.join("").toString()
-
 todaysTasks.innerHTML = taskArrayHTML
-
 dayOnCard.innerHTML = cardData.day
-/*Actions */
 
 /*StopWatch Functionality */
 
@@ -119,41 +120,41 @@ function pad(unit) {
 
 /* Task and time submit Functionality */
 taskInput.addEventListener("submit", (e) => {
-  /* e.preventDefault() */
-  let hrs = today.getHours().toString()
-  let minutes = today.getMinutes().toString()
-  minutes = pad(minutes)
-  let dateFormatted = hrs.concat(`:${minutes}`)
+  if (sendInputValue.value == "") {
+    e.preventDefault()
+    alert("You didn't provide title for your task!")
+  } else if (secs == 0 && mins == 0 && hours == 0) {
+    e.preventDefault()
+    alert("No time spent on a task")
+  } else {
+    let hrs = today.getHours().toString()
+    let minutes = today.getMinutes().toString()
+    minutes = pad(minutes)
+    let dateFormatted = hrs.concat(`:${minutes}`)
 
-  data = {
-    time: dateFormatted,
-    secs: parseFloat(secs),
-    mins: parseFloat(mins),
-    hours: parseFloat(hours),
-    status: sendInputValue.value,
+    data = {
+      time: dateFormatted,
+      secs: pad(secs),
+      mins: pad(mins),
+      hours: parseFloat(hours),
+      status: sendInputValue.value,
+    }
+
+    /*Clearing Interface */
+    paused = true
+    clearInterval(intervalId)
+    startTime = 0
+    elapsedTime = 0
+    currentTime = 0
+    secs = 0
+    mins = 0
+    hours = 0
+    timeDisplay.textContent = `00:00:00`
+
+    /*Updating cardData*/
+    let { day, taskList } = cardData
+    taskList = [...taskList, data]
+    let taskList_serialized = JSON.stringify(taskList)
+    localStorage.setItem("tasklist_serialized", taskList_serialized)
   }
-
-  /*Clearing Interface */
-  paused = true
-  clearInterval(intervalId)
-  startTime = 0
-  elapsedTime = 0
-  currentTime = 0
-  secs = 0
-  mins = 0
-  hours = 0
-  timeDisplay.textContent = `00:00:00`
-
-  /*Updating cardData*/
-  let { day, taskList } = cardData
-  taskList = [...taskList, data]
-  let taskList_serialized = JSON.stringify(taskList)
-  localStorage.setItem("tasklist_serialized", taskList_serialized)
 })
-
-/*Saving info in localStorage */
-/* let { day, taskList } = cardData
-let taskList_serialized = JSON.stringify(taskList)
-localStorage.setItem("tasklist_serialized", taskList_serialized) */
-
-/*Import-Export Data Functionality */
